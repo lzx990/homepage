f8ab678e1a4a0232792048416299ae179a13f196
lzx990 <105957483+lzx990@users.noreply.github.com>
2022-12-30 11:58:10 +0800

    Add files via upload

---
+++ bookmarks.js
@@ -0,0 +1,36 @@
+// Note: having length != 4 will mess with layout based on how the site is styled
+const bookmarks = [
+  {
+    title: "本站导航",
+    links: [
+      { name: "博客", url: "http://wiki.lzx19.top" },
+      { name: "个人介绍", url: "/about" },
+      { name: "友链", url: "/friend-link" },
+    ],
+  },
+  {
+    title: "我的踪迹",
+    links: [
+      { name: "Email", url: "mailto:lzx@czoier.top" },
+      { name: "Luogu", url: "https://www.luogu.com.cn/user/426624" },
+      { name: "GitHub", url: "https://github.com/lzx990/" },
+      { name: "Codeforces", url: "https://codeforces.com/profile/lzx19" },
+      { name: "AcWing", url : "https://www.acwing.com/user/myspace/index/110887/" },
+      { name: "HydroOJ", url : "https://hydro.ac/user/13444" },
+      { name: "LOJ", url : "https://loj.ac/u/lzx1609" },
+      { name: "Gitee", url : "https://gitee.com/lzx2209/" },
+    ],
+  },
+  {
+    title: "项目们",
+    links: [
+      { name: "CZJOI", url: "https://hydro.ac/d/czjoi/" },
+    ],
+  },
+  {
+    title: "其他",
+    links: [
+      { name: "OI交流群", url: "/oi-chat" },
+    ],
+  },
+];
